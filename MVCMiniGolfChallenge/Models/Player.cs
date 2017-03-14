db6b68c29d03c2ea904a0682f22ae2fb04f1ac4d
washes5 <angiewashburn1997@gmail.com>
2017-03-13 21:05:17 -0400

    Player Class and Players Database

---
+++ MVCMiniGolfChallenge/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCMiniGolfChallenge.Models
+{
+    public class Player
+    {
+        public int PlayerID { get; set; }
+        public string PlayerLastName { get; set; }
+        public string PlayerFirstName { get; set; }
+        public int PlayerScore { get; set; }
+    }
+
+    public class PlayerDBContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+    }
+}
